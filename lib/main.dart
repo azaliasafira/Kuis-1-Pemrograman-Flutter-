747be7117b92fbfeb94f86921f3436e4d8f66de2
= <=>
2021-03-14 09:42:52 +0700

    UI: data siswa

---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  String jk = "";
+  String dropdownValue = 'Teknologi Informasi';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Data Mahasiswa"),
+        ),
+        body: ListView(
+          children: [
+            Container(
+              margin: EdgeInsets.all(8),
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'NIM', hintText: 'masukan nim'),
+                    keyboardType: TextInputType.numberWithOptions(
+                      decimal: true,
+                      signed: false,
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Nama Lengkap ',
+                        hintText: 'masukan nama lengkap'),
+                  ),
+                  TextFormField(
+                    maxLines: 2,
+                    decoration: InputDecoration(
+                        labelText: 'Alamat', hintText: 'masukan alamat'),
+                  ),
+                  new Padding(
+                    padding: new EdgeInsets.only(top: 20.0),
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        child: Text('Pilih Jenis Kelamin'),
+                      ),
+                      RadioListTile(
+                        value: "Laki - Laki",
+                        title: new Text("Laki-Laki"),
+                        groupValue: jk,
+                        onChanged: (String value) {
+                          pilihJk(value);
+                        },
+                      ),
+                      RadioListTile(
+                        value: "Perempuan",
+                        title: new Text("Perempuan"),
+                        groupValue: jk,
+                        onChanged: (String value) {
+                          pilihJk(value);
+                        },
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        child: Text('Pilih Jurusan'),
+                      ),
+                      DropdownButton<String>(
+                        value: dropdownValue,
+                        icon: Icon(Icons.arrow_downward),
+                        iconSize: 24,
+                        elevation: 16,
+                        style: TextStyle(color: Colors.black),
+                        underline: Container(
+                          height: 2,
+                          color: Colors.blueAccent,
+                        ),
+                        onChanged: (String newValue) {
+                          setState(() {
+                            dropdownValue = newValue;
+                          });
+                        },
+                        items: <String>[
+                          'Teknologi Informasi',
+                          'Elektronika',
+                          'Akuntansi'
+                        ].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(15),
+                        child: new ButtonBar(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            RaisedButton(
+                              child: new Text('Create'),
+                              color: Colors.blueAccent,
+                              onPressed: () {/** */},
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void pilihJk(String value) {
+    setState(() {
+      jk = value;
+    });
+  }
